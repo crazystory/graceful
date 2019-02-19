fb2d40d16ca29fc09a50e02536b312abafe28963
CaoYong <caoyong2619@gmail.com>
2019-02-19 16:08:27 +0800

    修正无法启动多个server的问题

--- graceful.go
+++ graceful.go
@@ -10,28 +10,34 @@ import (
 	"syscall"
 )
 
+var (
+	servers = make(map[Server]net.Listener, 0)
+)
+
 type Server interface {
 	Listener() (net.Listener, error)
 	Startup(ln net.Listener)
 	Shutdown(ctx context.Context)
 }
 
-func Startup(s Server) error {
-	ln, err := ensureListener(s)
-	if nil != err {
-		return err
+func Startup(servers ...Server) error {
+	for _, s := range servers {
+		ln, err := ensureListener(s)
+		if nil != err {
+			return err
+		}
+
+		startup(s, ln)
 	}
-
-	startup(s, ln)
-
-	return wait(s, ln)
+	return nil
 }
 
 func startup(s Server, ln net.Listener) {
+	servers[s], _ = s.Listener()
 	go s.Startup(ln)
 }
 
-func wait(s Server, ln net.Listener) error {
+func Wait() error {
 	sign := make(chan os.Signal)
 	signal.Notify(sign, syscall.SIGUSR2)
 
@@ -40,7 +46,7 @@ func wait(s Server, ln net.Listener) error {
 		case sig := <-sign:
 			switch sig {
 			case syscall.SIGUSR2:
-				if err := restart(s, ln); err != nil {
+				if err := restart(); err != nil {
 					return err
 				}
 			}
@@ -48,72 +54,86 @@ func wait(s Server, ln net.Listener) error {
 	}
 }
 
-func fork(ln net.Listener) (*os.Process, error) {
-	lnFile, err := listenerFile(ln)
+func fork() error {
+	files := []uintptr{
+		os.Stdin.Fd(),
+		os.Stdout.Fd(),
+		os.Stdout.Fd(),
+	}
+	for _, ln := range servers {
+		if ln != nil {
+			lnFile, err := listenerFile(ln)
+			if err != nil {
+				return err
+			}
+			files = append(files, lnFile.Fd())
 
-	if nil != err {
-		return nil, err
+			if err := os.Setenv(`_LISTENER_`+ln.Addr().String(), lnFile.Name()); err != nil {
+				return err
+			}
+		}
 	}
 
-	defer lnFile.Close()
-	files := []*os.File{
-		os.Stdin,
-		os.Stdout,
-		os.Stderr,
-		lnFile,
-	}
-
+	env := os.Environ()
 	exec, err := os.Executable()
 
 	if nil != err {
-		return nil, err
+		return err
 	}
 
 	dir := filepath.Dir(exec)
-	env := append(os.Environ(), `LISTENER_FILE=`+lnFile.Name())
 
-	process, err := os.StartProcess(exec, []string{exec}, &os.ProcAttr{
+	_, err = syscall.ForkExec(os.Args[0], os.Args, &syscall.ProcAttr{
 		Dir:   dir,
 		Env:   env,
 		Files: files,
-		Sys:   &syscall.SysProcAttr{},
 	})
-	if nil != err {
-		return nil, err
-	}
 
-	return process, nil
+	return err
 }
 
 func listenerFile(ln net.Listener) (*os.File, error) {
+	if nil == ln {
+		return nil, nil
+	}
+
 	switch t := ln.(type) {
 	case *net.TCPListener:
 		return t.File()
 	case *net.UnixListener:
 		return t.File()
+	default:
+		return nil, errors.New(`unsupported listener`)
 	}
-	return nil, errors.New(`unsupported listener`)
 }
 
-func restart(s Server, ln net.Listener) error {
-	if _, err := fork(ln); err != nil {
+func restart() error {
+	if err := fork(); err != nil {
 		return err
 	}
 
-	return syscall.Kill(os.Getpid(), syscall.SIGTERM)
+	if p, err := os.FindProcess(os.Getpid()); err != nil {
+		return err
+	} else {
+		return p.Kill()
+	}
 }
 
 func ensureListener(s Server) (net.Listener, error) {
-	env := os.Getenv(`LISTENER_FILE`)
+	ln, has := servers[s]
 
-	if env != `` {
-		lnFile := os.NewFile(3, env)
-		if lnFile == nil {
-			return nil, errors.New(`unable to created listener file ` + env)
+	if has {
+		env := os.Getenv(`_LISTENER_` + ln.Addr().String())
+
+		if env != `` {
+			lnFile := os.NewFile(3, env)
+			if lnFile == nil {
+				return nil, errors.New(`unable to created listener file ` + env)
+			}
+			defer lnFile.Close()
+
+			return net.FileListener(lnFile)
 		}
-		defer lnFile.Close()
-
-		return net.FileListener(lnFile)
 	}
 
 	return s.Listener()
